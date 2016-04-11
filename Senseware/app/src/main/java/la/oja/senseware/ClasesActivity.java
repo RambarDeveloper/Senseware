58bfa41fc89bd843ed1fe63ad6d9f75d3b091871
rambar <rambardeveloper@gmail.com>
2016-04-11 16:23:44 -0430

    creando barra superior e inferior de activity_clases.xml

--- Senseware/app/src/main/java/la/oja/senseware/ClasesActivity.java
+++ Senseware/app/src/main/java/la/oja/senseware/ClasesActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,25 +33,33 @@ public class ClasesActivity extends AppCompatActivity {
 
 
         for(int i=0; i<4; i++){
-            LinearLayout linearLayout = new LinearLayout(this);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            //params.width=1;
-            linearLayout.setLayoutParams(params);
-            linearLayout.setId(i);
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            //params.gravity= Gravity.CENTER_HORIZONTAL;
 
+            //Creando LinearLayout (contenedor) para cada uno de los emprendedores
+            LinearLayout emprendedorLayout = new LinearLayout(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            emprendedorLayout.setLayoutParams(params);
+            emprendedorLayout.setId(i);
+            emprendedorLayout.setOrientation(LinearLayout.VERTICAL);
+
+            //Creando ImageView para mostrar imagen de cada emprendedor
             ImageView imagen = new ImageView(this);
             LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(200, 200);
             imageParams.gravity=Gravity.CENTER_HORIZONTAL;
             imagen.setLayoutParams(imageParams);
             imagen.setId(i * 100);
             imagen.setImageResource(R.drawable.sw_white);
+            emprendedorLayout.addView(imagen); //agregando imagen al LinearLayout
 
+            //TextView
+            TextView textoNombre = new TextView(this);
+            LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            textParams.gravity=Gravity.CENTER_HORIZONTAL;
+            textoNombre.setLayoutParams(textParams);
+            textoNombre.setText("Prueba " + i);
+            textoNombre.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+            emprendedorLayout.addView(textoNombre);
 
-            linearLayout.addView(imagen);
-
-            listaDeClases.addView(linearLayout);
+            listaDeClases.addView(emprendedorLayout);
         }
     }
 }
