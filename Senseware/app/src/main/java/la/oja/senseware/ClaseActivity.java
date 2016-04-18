0708a9fbcfed3fcd261f3ce7564ab5700f670cb2
rambar <rambardeveloper@gmail.com>
2016-04-18 16:57:18 -0430

    Agregando actividad AudioClaseActivity

---
+++ Senseware/app/src/main/java/la/oja/senseware/ClaseActivity.java
@@ -0,0 +1,13 @@
+package la.oja.senseware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clase);
+    }
+}
