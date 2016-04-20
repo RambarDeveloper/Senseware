a864f6da18dbc2abb0a44bc745f517de08072b2b
rambar <rambardeveloper@gmail.com>
2016-04-20 16:03:22 -0430

    agregando RespuestaActivity

---
+++ Senseware/app/src/main/java/la/oja/senseware/RespuestaActivity.java
@@ -0,0 +1,13 @@
+package la.oja.senseware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RespuestaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_respuesta);
+    }
+}
