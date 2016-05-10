ea2e1817109cb4e30705e3f68ead9a5638be2809
Juan Robles <robles.juanc@gmail.com>
2016-05-10 15:41:53 -0400

    youtube

---
+++ Senseware/app/src/main/java/la/oja/senseware/Config.java
@@ -0,0 +1,13 @@
+package la.oja.senseware;
+
+/**
+ * Created by jcoaks on 10/5/2016.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCy-p3FULbrRMU3FSLaqJ3BzW6KyJgaduQ";
+
+}
--- Senseware/app/src/main/java/la/oja/senseware/MainActivity.java
+++ Senseware/app/src/main/java/la/oja/senseware/MainActivity.java
@@ -11,11 +11,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-import java.net.URI;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +31,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
-
         //Reproducir video
-       loadVideo();
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+
+        loadVideo();
     }
 
     @Override
@@ -36,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Metodo para reproducir video (loop)
     public void loadVideo(){
-        VideoView videoView = (VideoView) findViewById(R.id.video);
+       /* VideoView videoView = (VideoView) findViewById(R.id.video);
         Uri path = Uri.parse("android.resource://la.oja.senseware/" + R.raw.senseware);
         videoView.setVideoURI(path);
         videoView.start();
@@ -45,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onPrepared(MediaPlayer mp) {
                 mp.setLooping(true);
             }
-        });
+        });*/
     }
 
     @Override
@@ -74,5 +84,39 @@ public class MainActivity extends AppCompatActivity {
     public void inicioSesion(View view) {
        Intent intento = new Intent(this, ClasesActivity.class);
        startActivity(intento);
+    }
+
+    //////////////////////////////////// Youtube
+
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            //player.setFullscreen(true);
+            player.setPlayerStyle(YouTubePlayer.PlayerStyle.MINIMAL);
+            player.cueVideo("kEpOF7vUymc"); // Plays https://www.youtube.com/watch?v=kEpOF7vUymc
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
